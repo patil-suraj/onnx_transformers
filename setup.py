6eca890e6d6065f8e8c3a56ef790e76ccdc07fce
Suraj Patil <surajp815@gmail.com>
2020-09-05 21:53:34 +0530

    update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from setuptools import setup, find_packages
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
+with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 extras = {}
